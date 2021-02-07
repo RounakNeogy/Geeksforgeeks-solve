43ae01eb7635f364834f4b20b4d1d3050e2b1ea6
Rounak Neogy <56738141+RounakNeogy@users.noreply.github.com>
2021-02-08 00:00:31 +0530

    Update Find transition point.cpp

--- Arrays/Find transition point.cpp
+++ Arrays/Find transition point.cpp
@@ -21,7 +21,18 @@ Explanation: Since, there is no "1",
 the answer is -1.
 
 Your Task:
-You don't need to read input or print anything. The task is to complete the function transitionPoint() that takes array and N as input parameters and returns the 0 based index of the position where "0" ends and "1" begins. If array does not have any 1s, return -1. If array does not have any 0s, return 0.
+You don't need to read input or print anything.
+The task is to complete the function transitionPoint() that takes array and N as input parameters and returns the 0 based index of the position where "0" ends and "1" begins.
+If array does not have any 1s, return -1.
+If array does not have any 0s, return 0.
+
+Expected Time Complexity: O(LogN)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 ≤ N ≤ 500000
+0 ≤ arr[i] ≤ 1
 */
 
 
